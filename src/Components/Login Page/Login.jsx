dff8f9857516fb47d778e969737c40d7afafb73f
SandeepKumar4567 <158554567+SandeepKumar4567@users.noreply.github.com>
2024-09-29 11:24:42 +0530

    Add files via upload

---
+++ src/Components/Login Page/Login.jsx
@@ -0,0 +1,69 @@
+// src/Login.js
+import React, { useState } from 'react';
+import Home from '../Home Page/Home';
+import Signup from '../SignUp Page/SignUp';
+import Logo from './Logo';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [signup, setSignup] = useState(false);
+    const [tab, setTab] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username === 'admin' && password === '123') {
+            setTab(true);
+            alert('Login successful!');
+        } else {
+            alert('⚠️ Please Check Username and Password');
+        }
+    };
+    if (tab) {
+        return <Home />
+    }
+
+    function handleClick2() {
+        setSignup(true);
+    }
+    if (signup) {
+        return <Signup />
+    }
+
+    return (
+        <>
+        < Logo />
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <div className="login-section">
+                    <h1>LOGIN</h1>
+                    <label className='username'>Username (admin)</label>
+                    <input
+                        type="text"
+                        placeholder='admin'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <br />
+                    <label>Password (123)</label>
+                    <input
+                        type="password"
+                        placeholder='123'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
+                    <br />
+                        <button onClick={handleSubmit}>Sign In</button>
+                    <br />
+                    <p>Dont't Have An Account ?</p>
+                    <div onClick={handleClick2}>Sign up</div>
+                    <br />
+                    <br />
+                </div>
+            </form>
+        </div>
+        </>
+    );
+};
+
+
+export default Login;
